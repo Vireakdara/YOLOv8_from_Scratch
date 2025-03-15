03ce7b098e10c22d178da30ac4f739bc78569518
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-15 23:51:02 +0700

    Update model.py

--- model.py
+++ model.py
@@ -255,3 +255,26 @@ class Neck(nn.Moduel):
         self.cv_1=Conv(in_channels=int(256*w),out_channels=int(256*w),kernel_size=3,stride=2, padding=1)
         self.cv_2=Conv(in_channels=int(512*w),out_channels=int(512*w),kernel_size=3,stride=2, padding=1)
 
+    def forward(self, x_res_1, x_res_2, x): # x_res_1, x_res_2, x = ouptut of the backbone 
+        res_1 = x               # for residual connection
+        x = self.up(x)
+        x = torch.cat([x, x_res_2], dim=1)
+
+        res_2 = self.c2f_1(x)   # fro residual connection
+
+        x = self.up(res_2)
+        x = torch.cat([x, x_res_1], dim=1)
+
+        out_1 = self.c2f_2(x)
+
+        x = self.cv_1(out_1)
+
+        x = torch.cat([x, res_2], dim=1)
+        out_2 = self.c2f_3(x)
+
+        x = self.cv_2(out2)
+
+        x = torch.cat([x, res_1], dim=1)
+        out_3 = self.c2f_4(x)
+
+        return out_1,out_2,out_3
